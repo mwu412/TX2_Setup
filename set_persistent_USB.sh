4be1451aef91b076ba572c01160eb2f3b909806e
mobydickhm1851 <37865338+mobydickhm1851@users.noreply.github.com>
2018-05-25 16:31:18 +0800

    Update set_persistent_USB.sh

--- set_persistent_USB.sh
+++ set_persistent_USB.sh
@@ -8,5 +8,7 @@
 sudo touch /etc/udev/rules.d/99-usb-serial.rules
 # Add content #
 sudo bash -c "echo 'SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", SYMLINK+=\"solabot_nano\"' >> /etc/udev/rules.d/99-usb-serial.rules"
+sudo bash -c "echo 'SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"2341\", ATTRS{idProduct}==\"8037\", SYMLINK+=\"solabot_micro\"' >> /etc/udev/rules.d/99-usb-serial.rules"
+sudo bash -c "echo 'KERNEL==\"ttyACM*\", ATTRS{idVendor}==\"2341\",SYMLINK+=\"solabot_micro\"' >> /etc/udev/rules.d/99-usb-serial.rules"
 # load the new rule #
 sudo udevadm trigger
