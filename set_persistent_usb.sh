d3fc63e6be0ceaa0726240d6d0ef928e67a259a0
mobydickhm1851 <37865338+mobydickhm1851@users.noreply.github.com>
2018-05-25 12:34:39 +0800

    Create set_persistent_usb.sh

---
+++ set_persistent_usb.sh
@@ -0,0 +1,12 @@
+# Give persistent name for usb_devices (arduino nano and micro) #
+# source:https://bit.ly/2Lt6NPU #
+
+# lsusb to identify the VendorID and ProductID #
+# In the example "Bus 001 Device 005: ID 0403:6001" 0403 is the VendorID and 6001 the ProductID #
+
+# Create a file #
+sudo touch /etc/udev/rules.d/99-usb-serial.rules
+# Add content #
+sudo bash -c "echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", SYMLINK+="solabot_nano"' >> /etc/udev/rules.d/99-usb-serial.rules"
+# load the new rule #
+sudo udevadm trigger
