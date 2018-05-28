56feb4a4e99ffded2ac6f8d17553cc2b1f6ef191
mobydickhm1851 <37865338+mobydickhm1851@users.noreply.github.com>
2018-05-28 10:10:55 +0800

    Create sensor_packages_install.sh

---
+++ sensor_packages_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+###-- this file mainly install packages for sensors --### 
+
+###-- ======= --###
+###-- rplidar --###
+###-- ======= --###
+sudo apt-get install ros-kinetic-rplidar-ros
+
