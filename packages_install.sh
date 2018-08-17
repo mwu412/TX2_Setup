6a58058ccd3fc64471d0beb821e5b00f1ea2ab07
mobydickhm1851 <37865338+mobydickhm1851@users.noreply.github.com>
2018-08-17 12:33:55 +0800

    Update packages_install.sh

--- packages_install.sh
+++ packages_install.sh
@@ -1,49 +1,40 @@
 #!/bin/bash
 ###-- this file mainly install packages for sensors or navigation packages --### 
 
-###-- ======= --###
 ###-- rplidar --###
 ###-- ======= --###
 sudo apt-get install ros-kinetic-rplidar-ros
 
-###-- ============== --###
 ###-- razor_imu_9DOF --###
 ###-- ============== --###
 sudo apt-get install ros-kinetic-razor-imu-9dof
 
-###-- ==== --###
 ###-- rf2o --###
 ###-- ==== --###
 sudo apt-get install ros-kinetic-mrpt-ekf-slam-2d
 
-###-- ========= --###
 ###-- move_base --###
 ###-- ========= --###
 sudo apt-get install ros-kinetic-move-base
 
-###-- ==== --###
 ###-- amcl --###
 ###-- ==== --###
 sudo apt-get install ros-kinetic-amcl
 
-###-- ================== --###
 ###-- robot_localization --###
 ###-- ================== --###
 sudo apt-get install ros-kinetic-robot-localization
 
-###-- === --###
 ###-- rqt --###
 ###-- === --###
 sudo apt-get install ros-kinetic-rqt
 sudo apt-get install ros-kinetic-rqt-common-plugins
 
-###-- =================================== --###
 ###-- mrpt (rf2o is part of this package) --###
 ###-- =================================== --###
 sudo apt-get install mrpt-apps libmrpt-dev
 # see trouble shooting if something is wrong #
 
-###-- ================================================ --###
 ###-- effort_controllers (solabot_simulator in gazebo) --###
 ###-- ================================================ --###
 sudo apt-get install ros-kinetic-effort-controllers
