b5ed4ac5fcb2911c1684719567e68efca8051ec5
mwu412 <mwu406@yahoo.com>
2018-12-14 11:04:00 +0800

    add apt-get "-y"

--- packages_install.sh
+++ packages_install.sh
@@ -4,17 +4,17 @@
 
 ###-- basic ros command and package --###
 # rosbash, map_server
-sudo apt-get install ros-kinetic-rosbash ros-kinetic-map-server
+sudo apt-get -y install ros-kinetic-rosbash ros-kinetic-map-server
 
 ###-- joy --###
-sudo apt-get install ros-kinetic-joy
+sudo apt-get -y install ros-kinetic-joy
 
 ###-- rplidar --###
-sudo apt-get install ros-kinetic-rplidar-ros
+sudo apt-get -y install ros-kinetic-rplidar-ros
 # INSTRUCTION: Remember to comment one of the two lidars in solabot/src/solabot_navigation/launch/solabot_core.launch
 
 ###-- urg_node (hokuyo lidar) --###
-sudo apt-get install ros-kinetic-urg-node
+sudo apt-get -y install ros-kinetic-urg-node
 # INSTRUCTION: Remember to comment one of the two lidars in solabot/src/solabot_navigation/launch/solabot_core.launch
 
 # (Alternative for urg_node) From source:
@@ -32,54 +32,54 @@ sudo apt-get install ros-kinetic-urg-node
 #catkin_make
 
 ###-- razor_imu_9DOF --###
-sudo apt-get install ros-kinetic-razor-imu-9dof
+sudo apt-get -y install ros-kinetic-razor-imu-9dof
 # INSTRUCTION: Copy solabot/src/solabot_navigation/scripts/solabot_imu.py to /opt/ros/kinetic/share/razor_imu_9dof/nodes
 
 ###-- rf2o --###
-sudo apt-get install ros-kinetic-mrpt-ekf-slam-2d
+sudo apt-get -y install ros-kinetic-mrpt-ekf-slam-2d
 
 ###-- move_base --###
 ### move_base 
-sudo apt-get install ros-kinetic-move-base
+sudo apt-get -y install ros-kinetic-move-base
 ### dwa_local_planner 
-sudo apt-get install ros-kinetic-dwa-local-planner
+sudo apt-get -y install ros-kinetic-dwa-local-planner
 ### teb_local_planner 
-sudo apt-get install ros-kinetic-teb-local-planner
+sudo apt-get -y install ros-kinetic-teb-local-planner
 ### global_planner
-sudo apt-get install ros-kinetic-global-planner
+sudo apt-get -y install ros-kinetic-global-planner
 
 ###-- amcl --###
-sudo apt-get install ros-kinetic-amcl
+sudo apt-get -y install ros-kinetic-amcl
 
 ###-- robot_localization --###
-sudo apt-get install ros-kinetic-robot-localization
+sudo apt-get -y install ros-kinetic-robot-localization
 
 ###-- rqt --###
-sudo apt-get install ros-kinetic-rqt
-sudo apt-get install ros-kinetic-rqt-common-plugins
+sudo apt-get -y install ros-kinetic-rqt
+sudo apt-get -y install ros-kinetic-rqt-common-plugins
 
 ###-- mrpt (rf2o is part of this package) --###
-sudo apt-get install mrpt-apps libmrpt-dev
+sudo apt-get -y install mrpt-apps libmrpt-dev
 # see trouble shooting if something is wrong #
 
 ###-- effort_controllers (solabot_simulator in gazebo) --###
-sudo apt-get install ros-kinetic-effort-controllers
+sudo apt-get -y install ros-kinetic-effort-controllers
 
 ### rosserial-python
-sudo apt-get install ros-kinetic-rosserial-python
+sudo apt-get -y install ros-kinetic-rosserial-python
 
 ### rosserial-arduino
-sudo apt-get install ros-kinetic-rosserial-arduino
+sudo apt-get -y install ros-kinetic-rosserial-arduino
 
 ### ros-kinetic-joy
-sudo apt-get install ros-kinetic-joy
+sudo apt-get -y install ros-kinetic-joy
 
 ###-- joint_state_controller --###
-sudo apt-get install ros-kinetic-joint-state-controller
+sudo apt-get -y install ros-kinetic-joint-state-controller
 
 ###-- slam-gmapping --###
-sudo apt-get install ros-kinetic-slam-gmapping
+sudo apt-get -y install ros-kinetic-slam-gmapping
 
 ###-- openssh --###
-apt-get install openssh-server
+apt-get -y install openssh-server
 # both TX2 and the remote computer need to install
